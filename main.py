3e8c045554b5f2971bc1c2fd819a4871dfe8169b
uvvhfbdhfdgf <2821172548@qq.com>
2026-03-23 18:47:02 +0800

    Disable slider verification and update loop condition

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ get_current_dayofweek = lambda action: (
 SLEEPTIME = 0.2  # 每次抢座的间隔
 ENDTIME = "20:01:00"  # 根据学校的预约座位时间+1min即可
 
-ENABLE_SLIDER = True  # 是否有滑块验证
+ENABLE_SLIDER = False  # 是否有滑块验证
 MAX_ATTEMPT = 5  # 最大尝试次数
 RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
 
@@ -88,7 +88,7 @@ def main(users, action=False):
     target_wait=0
     logging.info(f"等待到 {target_hour:02d}:{target_minute:02d}:{target_second:02d} 再开始抢座...")
 
-    while False:
+    while True:
         now_ts = time.time() + (8 * 3600 if action else 0)
         now = time.localtime(now_ts)
         if (now.tm_hour == target_hour and
